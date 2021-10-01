9e9f47c3019c14b8bda5ba8489ea67687a38270b
Choi JIyeon <81598401+jiyeon29@users.noreply.github.com>
2021-10-02 00:58:57 +0900

    Update 6001~6026.py

--- Python/CodeUp/Basic_100/6001~6026.py
+++ Python/CodeUp/Basic_100/6001~6026.py
@@ -1,3 +1,5 @@
+# 21.10.01. 풀이
+
 # 6001. 출력하기01
 # print( )를 이용해 다음 단어를 출력하시오.
 # Hello
@@ -23,7 +25,22 @@ World
 print("Hello")
 print("World")
 
+# 추가 답안) print("Hello\nWorld")
 
-# 6001. 출력하기01
-# 6001. 출력하기01
+
+# 6004. 출력하기04
+# 작은 따옴표(')(single quotation mark)가 들어있는 문장을 출력하시오.
+# 'Hello'
+
+print("'Hello'")
+
+
+# 6005. 출력하기05
+# 큰따옴표(")(double quotation mark)가 포함된 문장을 출력하시오.
+# "Hello World" (단, 큰따옴표도 함께 출력한다.)
+
+print('"Hello World"')
+
+# 추가 답안) print("\"Hello World\"")
+
 # 6001. 출력하기01
